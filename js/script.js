5b216e981ccbb9f4fad6a7bcc8a5b0aa7dc72d6a
pcamaralblueberrygp <pcamaral.blueberrygp@gmail.com>
2026-06-02 12:42:04 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('Blueberry');
+document.addEventListener("DOMContentLoaded", () => {
+
+  const form = document.querySelector("form");
+
+  if(form){
+
+    form.addEventListener("submit", () => {
+
+      const button = form.querySelector("button");
+
+      button.innerText = "Enviando...";
+
+      button.disabled = true;
+
+    });
+
+  }
+
+});
